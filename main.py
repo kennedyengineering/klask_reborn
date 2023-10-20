84112c0724594c45c7129d8b79039ec6a0a734da
no <no>
2023-10-19 18:36:45 -0700

    colors added

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from klask_constants import *
 # --- constants ---
 # Box2D deals with meters, but we want to display pixels,
 # so define a conversion factor:
-# PPM = 20.0  # pixels per meter
 PPM = 2000
 TARGET_FPS = 60
 TIME_STEP = 1.0 / TARGET_FPS
@@ -20,7 +19,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = KG_BOARD_WIDTH * PPM, KG_BOARD_HEIGHT * PPM
 
 # --- pygame setup ---
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-pygame.display.set_caption('Simple pygame example')
+pygame.display.set_caption('Klask Simulator')
 clock = pygame.time.Clock()
 
 # --- pybox2d world setup ---
@@ -48,8 +47,7 @@ wall_top = world.CreateStaticBody(
 ground = world.CreateStaticBody(position=(0,0))
 ground_fixture = ground.CreatePolygonFixture(box=(KG_BOARD_WIDTH, KG_BOARD_HEIGHT), density=1, friction=0.3)
 
-# --- kinematic bodies ---
-
+# --- dynamic bodies ---
 puck1_body = world.CreateDynamicBody(
     position=(KG_BOARD_WIDTH / 3, KG_BOARD_HEIGHT / 2), 
     shapes=circleShape(radius=KG_BALL_RADIUS),
@@ -60,8 +58,6 @@ puck2_body = world.CreateDynamicBody(
     position=(2 * (KG_BOARD_WIDTH / 3), KG_BOARD_HEIGHT / 2), 
     shapes=circleShape(radius=KG_BALL_RADIUS)
 )
-
-# --- dynamic bodies ---
 
 ball_body = world.CreateDynamicBody(position=(KG_BOARD_WIDTH / 3, KG_BOARD_HEIGHT / 3))
 ball = ball_body.CreateCircleFixture(radius=KG_BALL_RADIUS, density=1, friction=0.3)
@@ -75,7 +71,7 @@ biscuit2 = biscuit2_body.CreateCircleFixture(radius=KG_BISCUIT_RADIUS, density=1
 biscuit3_body = world.CreateDynamicBody(position=(KG_BOARD_WIDTH / 2, (KG_BOARD_HEIGHT / 2) - KG_BISCUIT_START_OFFSET_Y))
 biscuit3 = biscuit3_body.CreateCircleFixture(radius=KG_BISCUIT_RADIUS, density=1, friction=0.3)
 
-# Create joint
+# --- create joints ---
 ball_ground_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=ball_body, maxForce=0.0015)
 biscuit1_ground_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=biscuit1_body, maxForce=0.0015)
 biscuit2_ground_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=biscuit2_body, maxForce=0.0015)
@@ -83,32 +79,10 @@ biscuit3_ground_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=biscuit3_b
 puck1_body_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=puck1_body, maxForce=0.0)
 puck2_body_joint = world.CreateFrictionJoint(bodyA=ground, bodyB=puck2_body, maxForce=0.0)
 
-colors = {
-    staticBody: (255, 255, 255, 255),
-    dynamicBody: (127, 127, 127, 255),
-    kinematicBody: (0,0,0)
-}
-
-def my_draw_polygon(polygon, body, fixture):
-    # vertices = [(body.transform * v) * PPM for v in polygon.vertices]
-    # vertices = [(v[0], SCREEN_HEIGHT - v[1]) for v in vertices]
-    # pygame.draw.polygon(screen, colors[body.type], vertices)
-    pass
-polygonShape.draw = my_draw_polygon
-
-def my_draw_edge(edge, body, fixture):
-    # vertices = [(body.transform * v) * PPM for v in polygon.vertices]
-    # vertices = [(v[0], SCREEN_HEIGHT - v[1]) for v in vertices]
-    # pygame.draw.polygon(screen, colors[body.type], vertices)
-    pass
-edgeShape.draw = my_draw_edge
-
-def my_draw_circle(circle, body, fixture):
-    position = body.transform * circle.pos * PPM
-    position = (position[0], SCREEN_HEIGHT - position[1])
-    pygame.draw.circle(screen, colors[body.type], [int(
-        x) for x in position], int(circle.radius * PPM))
-circleShape.draw = my_draw_circle
+def draw_circle_fixture(circle, color, pixels_per_meter, surface):
+    position = circle.body.transform * circle.shape.pos * pixels_per_meter
+    position = (position[0], surface.get_height() - position[1])
+    pygame.draw.circle(screen, color, [int(x) for x in position], int(circle.shape.radius * pixels_per_meter))
 
 # --- main game loop ---
 game_board = render_game_board(PPM)
@@ -137,15 +111,20 @@ while running:
     screen.blit(game_board, (0,0))
 
     # Draw the world
-    for body in world.bodies:
-        for fixture in body.fixtures:
-            fixture.shape.draw(body, fixture)
+
+    draw_circle_fixture(puck1_body.fixtures[0], KG_PUCK_COLOR, PPM, screen)
+    draw_circle_fixture(puck2_body.fixtures[0], KG_PUCK_COLOR, PPM, screen)
+    draw_circle_fixture(ball, KG_BALL_COLOR, PPM, screen)
+    draw_circle_fixture(biscuit1, KG_BISCUIT_COLOR, PPM, screen)
+    draw_circle_fixture(biscuit2, KG_BISCUIT_COLOR, PPM, screen)
+    draw_circle_fixture(biscuit3, KG_BISCUIT_COLOR, PPM, screen)
+
 
     # Apply forces
-    ball_body.ApplyForce(force=(0.0005, 0), point=ball.shape.pos, wake=True)
-    biscuit1_body.ApplyForce(force=(0.0015, 0), point=biscuit1.shape.pos, wake=True)
-    biscuit2_body.ApplyForce(force=(0.0015, 0), point=biscuit2.shape.pos, wake=True)
-    biscuit3_body.ApplyForce(force=(0.0015, 0), point=biscuit3.shape.pos, wake=True)
+    # ball_body.ApplyForce(force=(0.0005, 0), point=ball.shape.pos, wake=True)
+    # biscuit1_body.ApplyForce(force=(0.0015, 0), point=biscuit1.shape.pos, wake=True)
+    # biscuit2_body.ApplyForce(force=(0.0015, 0), point=biscuit2.shape.pos, wake=True)
+    # biscuit3_body.ApplyForce(force=(0.0015, 0), point=biscuit3.shape.pos, wake=True)
 
     # Make Box2D simulate the physics of our world for one step.
     world.Step(TIME_STEP, 10, 10)
