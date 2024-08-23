69be628f6a578be6fecdcd626ac5002d5b0b436e
Braedan Kennedy <bkennedy@toyon.com>
2024-08-23 11:55:52 -0700

    got testing working

--- src/KlaskLib/test/test_environment.py
+++ src/KlaskLib/test/test_environment.py
@@ -1,6 +1,7 @@
 from stable_baselines3.common.env_checker import check_env
 from ..environment.environment import KlaskEnv
 
-env = KlaskEnv()
 
-check_env(env)
+def test_sb3_env_checker():
+    env = KlaskEnv()
+    check_env(env)
