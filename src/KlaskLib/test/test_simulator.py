16a119ecf28423c6d8d27a7824bb2b03705b77f0
Braedan Kennedy <bkennedy@toyon.com>
2024-08-28 16:34:01 -0700

    added render_mode test cases

--- src/KlaskLib/test/test_simulator.py
+++ src/KlaskLib/test/test_simulator.py
@@ -11,9 +11,7 @@ def test_simulator_random_seed():
     iter = 15
     prev_states = []
     for _ in range(iter):
-        frame, game_states, agent_states = sim.reset(
-            seed=10, ball_start_position="random"
-        )
+        _, _, agent_states = sim.reset(seed=10, ball_start_position="random")
 
         prev_states.append(agent_states)
 
@@ -30,10 +28,109 @@ def test_simulator_random_no_seed():
     iter = 15
     prev_states = []
     for _ in range(iter):
-        frame, game_states, agent_states = sim.reset(
-            seed=None, ball_start_position="random"
-        )
+        _, _, agent_states = sim.reset(seed=None, ball_start_position="random")
 
         prev_states.append(agent_states)
 
     assert len(set(prev_states)) > 1
+
+
+def test_simulator_render_mode_metadata():
+    """
+    Determine if render_modes exists as class metadata
+    """
+
+    assert hasattr(KlaskSimulator, "render_modes")
+
+
+def test_simulator_render_mode_default():
+    """
+    Determine if default frame rendering behaves as expected
+    """
+
+    sim = KlaskSimulator()
+
+    assert sim.render_mode is None
+
+    frame, _, _ = sim.reset()
+
+    assert sim.screen is None
+
+    assert sim.clock is None
+
+    assert frame is None
+
+
+def test_simulator_render_mode_none():
+    """
+    Determine if disabling frame rendering behaves as expected
+    """
+
+    sim = KlaskSimulator(render_mode=None)
+
+    assert sim.render_mode is None
+
+    frame, _, _ = sim.reset()
+
+    assert sim.screen is None
+
+    assert sim.clock is None
+
+    assert frame is None
+
+
+def test_simulator_render_mode_rgb_array():
+    """
+    Determine if rgb_array frame rendering behaves as expected
+    """
+    from numpy import ndarray
+
+    sim = KlaskSimulator(render_mode="rgb_array")
+
+    assert sim.render_mode == "rgb_array"
+
+    frame, _, _ = sim.reset()
+
+    assert sim.screen is None
+
+    assert sim.clock is None
+
+    assert isinstance(frame, ndarray)
+
+
+def test_simulator_render_mode_human_unclocked():
+    """
+    Determine if human_unclocked frame rendering behaves as expected
+    """
+    from numpy import ndarray
+
+    sim = KlaskSimulator(render_mode="human_unclocked")
+
+    assert sim.render_mode == "human_unclocked"
+
+    frame, _, _ = sim.reset()
+
+    assert sim.screen is not None
+
+    assert sim.clock is None
+
+    assert isinstance(frame, ndarray)
+
+
+def test_simulator_render_mode_human():
+    """
+    Determine if human frame rendering behaves as expected
+    """
+    from numpy import ndarray
+
+    sim = KlaskSimulator(render_mode="human")
+
+    assert sim.render_mode == "human"
+
+    frame, _, _ = sim.reset()
+
+    assert sim.screen is not None
+
+    assert sim.clock is not None
+
+    assert isinstance(frame, ndarray)
