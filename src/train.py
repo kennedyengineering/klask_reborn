2848151c84429d03bea13b0a0e052466b5847201
Braedan Kennedy <bkennedy@toyon.com>
2024-09-02 13:20:27 -0700

    train script runs a vectorized environment

--- src/train.py
+++ src/train.py
@@ -1,12 +1,28 @@
 from stable_baselines3.a2c import A2C
-from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
 
 from gymnasium.wrappers.time_limit import TimeLimit
 
 from KlaskLib.environment.environment import KlaskEnv
 
-env = KlaskEnv(render_mode="human_unclocked")
-env = TimeLimit(env, max_episode_steps=1000)
-env = Monitor(env)
 
-model = A2C("CnnPolicy", env, verbose=1).learn(total_timesteps=100_000)
+def make_env():
+    def _init():
+
+        env = KlaskEnv(render_mode="human_unclocked")
+        env = TimeLimit(env, max_episode_steps=1000)
+
+        return env
+
+    return _init
+
+
+def main():
+    vec_env = SubprocVecEnv([make_env() for _ in range(5)])
+    vec_env = VecMonitor(vec_env)
+
+    model = A2C("CnnPolicy", vec_env, verbose=1).learn(total_timesteps=100_000)
+
+
+if __name__ == "__main__":
+    main()
